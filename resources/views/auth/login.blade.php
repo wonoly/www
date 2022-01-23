ff061239a5ff207ffa7222282bd6948c4453d081
mauro-balades <mauro.balades@tutanota.com>
2022-01-23 18:45:50 +0100

    added meta tags for SEO

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('template_title') Login @endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,40 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>@hasSection('template_title')@yield('template_title') | @endif {{ config('app.name', Lang::get('titles.app')) }}</title>
-        <meta name="description" content="">
-        <meta name="author" content="Jeremy Kenedy">
         <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="icon" href="/favicon.ico">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         {{-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --}}
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+
+        {{-- Meta tags --}}
+
+        <!-- Primary Meta Tags -->
+        <meta name="title" content="Wonoly - privacy is not a privilege, it is a right">
+        <meta name="description" content="Using Wonoly's products you will be safer than ever. Wonoly has a browser, search engine utility extensions and many more to make your experience on the internet better and more private.">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://wonoly.net/">
+        <meta property="og:title" content="Wonoly - privacy is not a privilege, it is a right">
+        <meta property="og:description" content="Using Wonoly's products you will be safer than ever. Wonoly has a browser, search engine utility extensions and many more to make your experience on the internet better and more private.">
+        <meta property="og:image" content="/images/meta.png" />
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://wonoly.net/">
+        <meta property="twitter:title" content="Wonoly - privacy is not a privilege, it is a right">
+        <meta property="twitter:description" content="Using Wonoly's products you will be safer than ever. Wonoly has a browser, search engine utility extensions and many more to make your experience on the internet better and more private.">
+        <meta property="twitter:image" content="/images/meta.png" />
+
+
+        <!-- Color -->
+        <meta name="theme-color" content="#fafafa">
 
         {{-- Fonts --}}
         @yield('template_linked_fonts')
